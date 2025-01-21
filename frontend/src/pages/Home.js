e7f5683fb832a42d4beea312f640010a9ba0f6b9
yurahriaziev <yura.hriaziev@gmail.com>
2025-01-21 12:00:54 -0500

    project initiated

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <div className="home-page">
+            <h1>Home Page</h1>
+            <h2>Upload 5 pictures of your car please</h2>
+        </div>
+    )
+}
